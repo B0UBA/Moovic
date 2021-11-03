ae2335018012c89eeb66f595790af1197f350b4e
Anaelle693 <anaellev@icloud.com>
2021-11-03 11:55:58 +0100

    modification BDD

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+
+
+#[Route('/admin')]
+class AdminController extends AbstractController
+{
+    #[Route('/', name: 'admin',methods: ['GET'])]
+    public function index(): Response
+    { 
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    #[Route('/user', name: 'admin_user',methods: ['GET'])]
+    public function showUser(UserRepository $userRepository): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+
+    }
+
+    #[Route('/{id}', name: 'user_delete', methods: ['POST'])]
+    public function deleteUser(Request $request, User $user): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('user_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    #[Route('/{id}/admin', name: 'user_delete_admin', methods: ['POST']),]
+    public function delete(Request $request, User $user): Response
+    {
+        if($this->isGranted('ROLE_ADMIN')){
+            if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->remove($user);
+                $entityManager->flush();
+            }
+        } else {
+            throw new AuthenticationException();
+        }
+        return $this->redirectToRoute('admin_user', [], Response::HTTP_SEE_OTHER);
+    }
+}
