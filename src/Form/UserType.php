f3d88a6d7aa2d84db4a040e593f521d6c9c65eb1
Anaelle693 <anaellev@icloud.com>
2021-11-03 15:15:14 +0100

    navbar admin et user minimum fonctionnel

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,13 @@ class UserType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('roles')
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Admin' => 'ROLE_ADMIN',
+                    'User' => 'ROLE_USER',
+                ],
+                "mapped" => false,
+            ])
             ->add('password')
             ->add('prenom')
             ->add('date_naissance')
--- src/Security/LoginFromAuthentificatorAuthenticator.php
+++ src/Security/LoginFromAuthentificatorAuthenticator.php
@@ -51,7 +51,7 @@ class LoginFromAuthentificatorAuthenticator extends AbstractLoginFormAuthenticat
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('app_login'));
+        return new RedirectResponse($this->urlGenerator->generate('accueil_index'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
